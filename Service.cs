9a1e8cf6c8038bff2b6087ccb9009a8f77ebe6a7
LeviAS <152180697+LeviAJs@users.noreply.github.com>
2026-02-21 23:44:53 -0300

    Service.cs

---
+++ Service.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System;
+
+
+
+namespace POO_treino
+    {
+    class Funcionario
+        {
+        private int Id { get; set; }
+        public int id => Id;
+        private string Name {get; set;}
+        public string nome => Name;
+        private float Salary {get; set;}
+        public float salario => Salary;
+
+        public Funcionario(int id, string name, float salary) {
+            this.Id = id;
+            this.Name = name;
+            this.Salary = salary;
+        
+            }
+        public Funcionario() { }
+
+
+        public float Receber(float salario) {
+            if(salario > 0) {
+                Salary += (salario/100);
+                }
+            return Salary;
+            }
+        }
+    }
+
