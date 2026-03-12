52320514312035a45d65f04d9128005b6253a83c
LeviAS <152180697+LeviAJs@users.noreply.github.com>
2026-03-11 22:33:10 -0300

    Update Service.cs
    refactor: melhora estrutura do Services.cs

--- Service.cs
+++ Service.cs
@@ -1,39 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Globalization;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System;
+using POO;
 
-
-
-namespace POO_treino
+namespace POO
     {
     class Funcionario
         {
-        private int Id { get; set; }
-        public int id => Id;
-        private string Name {get; set;}
-        public string nome => Name;
-        private float Salary {get; set;}
-        public float salario => Salary;
 
-        public Funcionario(int id, string name, float salary) {
+        private int Id
+            {
+            get; set;
+            }
+        public int id => Id;
+        private string Name
+            {
+            get; set;
+            }
+        public string nome => Name;
+        private decimal Salary
+            {
+            get; set;
+            }
+        public decimal salario => Salary;
+
+        public Funcionario()
+            {
+            }
+        public Funcionario(int id, string name, decimal salary)
+            {
             this.Id = id;
             this.Name = name;
             this.Salary = salary;
-        
             }
-        public Funcionario() { }
-
-
-        public float Receber(float salario) {
-            if(salario > 0) {
-                Salary += (salario/100);
+        public decimal AumentarSalarioPorPercentual(decimal salario)
+            {
+            if (salario > 0)
+                {
+                Salary += Salary * (salario / 100);
                 }
             return Salary;
             }
         }
     }
+
+
 
