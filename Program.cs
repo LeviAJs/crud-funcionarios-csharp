0ef8bc50f02ccc8c4d72b7acbe3f116ff32ee1c4
LeviAS <152180697+LeviAJs@users.noreply.github.com>
2026-03-11 22:28:21 -0300

    Update Program.cs
    refactor: reorganiza código em Program.cs

--- Program.cs
+++ Program.cs
@@ -8,13 +8,16 @@ using System.Globalization;
 namespace POO
 
     {
-    class programa {
-        static void Main(string[] args) {
-            var lista = new Repository();//instancia para armazernar dados 
-            int escolhaMenu = 0,id, aumento ;
-            bool validacao;
-            decimal salario;
-            while(true) { // inicio do loop
+    class programa
+        {
+        static void Main(string[] args)
+            {
+            var menu = new Menu();
+            var lista = new Repository();
+            int EscolhaMenu;
+            while (true)
+                {
+                Console.WriteLine();
                 Console.WriteLine("   Cadastro de usuario");
                 Console.WriteLine("----------------------------");
                 Console.WriteLine("1 - Cadastro");
@@ -23,59 +26,39 @@ namespace POO
                 Console.WriteLine("4 - Remover usuario");
                 Console.WriteLine("5 - Sair");
                 Console.Write("Escolha: ");
-                validacao = int.TryParse(Console.ReadLine(), out escolhaMenu);
-                if (validacao) {
-                    
-                        switch(escolhaMenu) {
-                            case 1:
-                                Console.WriteLine("Cadastro "); // cadastro de usuario
-                                Console.Write("Id: ");
-                                while(!int.TryParse(Console.ReadLine(), out id)) {
-                                    Console.WriteLine("Digite um número válido:");
-                                    }
-                                Console.Write("Entre com o nome: ");
-                                string nome = Console.ReadLine();
-                                Console.Write("Salario: ");
-                                while(!decimal.TryParse(Console.ReadLine(), out salario)) {
-                                    Console.WriteLine("Digite um número válido:");
-                                    }
-                                lista.Adicionar(new Funcionario(id, nome, salario));
-                                break;
-                            case 2:
-                                Console.WriteLine("Lista:"); // mostra de funcionarios
-                                lista.Mostar();
-                                break;
-                            case 3:
-                                Console.Write("ID do funcionario: ");// atualização do salario com o aumento
-                                while(!int.TryParse(Console.ReadLine(), out id)) {
-                                    Console.WriteLine("Digite um número válido:");
-                                    }
-                                Console.Write("Qual seria o aumento do salario(porcentual): ");
-                                while(!int.TryParse(Console.ReadLine(), out aumento)) {
-                                    Console.WriteLine("Digite um número válido:");
-                                    }
-                                lista.Aumento(id,aumento);
-                                Console.WriteLine("Aumento realizado com sucesso");
-                                break;
 
-                            case 4:
-                                Console.WriteLine("ID do usuario: ");// exclusao de usuario 
-                                id = int.Parse(Console.ReadLine());
-                                lista.Remover(id);
-                                Console.WriteLine("Removido com sucesso");
-                                break;
-                            default:
-                                // numero não cadastrado 
-                                Console.WriteLine("Numero fora fora da origem digite novamente");
-                                validacao = int.TryParse(Console.ReadLine(), out escolhaMenu);
-                                break;
-                            }
+                bool validacao = int.TryParse(Console.ReadLine(), out EscolhaMenu);
+
+                if (validacao)
+                    {
+                    switch (EscolhaMenu)
+                        {
+                        case 1:
+                            menu.Cadastro(lista);
+                            break;
+                        case 2:
+                            menu.MostarFuncionario(lista);
+                            break;
+                        case 3:
+                            menu.AumentoSalario(lista);
+                            break;
+                        case 4:
+                            menu.RemocaoFuncionario(lista);
+                            break;
+                        case 5:
+                           return;
+                        default:
+                            Console.WriteLine("Opção invalida");
+                            break;
                         }
-                if(!validacao) { // validacao deu erro por não ser numero
-                    Console.WriteLine("Precisa ser numero");
                     }
-                } 
-            }  
+                else
+                    {
+                    Console.WriteLine("Precisa estar em numero padrão");
+                    }
+
+                }
+            }
         }
     }
 
